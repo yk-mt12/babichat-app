9ae63bfc928903716fa018b6810e76775c1da119
yk-mt12 <agvksl543@gmail.com>
2022-08-13 03:50:10 +0900

    firebaseと連携

--- client/src/components/timeline/TimeLine.tsx
+++ client/src/components/timeline/TimeLine.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
 import Post from './Post'
 import PostBox from './PostBox'
 import './Timeline.css'
 
+import db from '../../firebase'
+import { collection, getDocs } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+
 const TimeLine = () => {
+  // TODO: 型定義を正しく行う
+  const [posts, setPosts] = useState<any>([])
+
+  useEffect(() => {
+    const postData = collection(db, 'posts')
+    getDocs(postData).then((querySnapshot) => {
+      setPosts(querySnapshot.docs.map((doc) => doc.data()))
+    })
+  }, [])
+
   return (
     <div className='timeline'>
       {/* Header */}
@@ -13,12 +26,17 @@ const TimeLine = () => {
       {/* PostBox */}
       <PostBox />
       {/* Post */}
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      {posts.map((post: any) => (
+        <Post
+          key={post.text}
+          displayName={post.displayName}
+          username={post.username}
+          verified={post.verified}
+          text={post.text}
+          avater={post.avater}
+          image={post.image}
+        />
+      ))}
     </div>
   )
 }
