3ef7f7c2f090f3b048879854ba69a3d65f25f9a1
yk-mt12 <agvksl543@gmail.com>
2022-08-13 01:23:59 +0900

    サイドバー（レイアウトのみ）を作成

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Sidebar from './components/sidebar/Sidebar'
 
 function App() {
-  return <div className='App'>hello</div>
+  return (
+    <div className='app'>
+      <Sidebar />
+    </div>
+  )
 }
 
 export default App
