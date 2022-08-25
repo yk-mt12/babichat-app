5c70e53f29aa4a889cadfb1d441b655bce9b133a
maple-fm <fuma0205boo@gmail.com>
2022-08-26 01:04:38 +0900

    チャット画面をスクロール可能にする

--- client/src/components/DM/ChatRoom.tsx
+++ client/src/components/DM/ChatRoom.tsx
@@ -1,5 +1,5 @@
 import { collection, limit, onSnapshot, orderBy, query } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { db } from '../../firebase'
 import { useAuth } from '../../firebase/authFunction'
 import Sidebar from '../sidebar/Sidebar'
@@ -18,20 +18,18 @@ const ChatRoom = () => {
     const [chats, setChats] = useState<any>([])
     const signInUser = useAuth()
     const uid = signInUser.uid
-    // console.log(uid)
+
     useEffect(() => {
         const anotherId = 'O1ujIkBZmJWXwdZi3htg5yai14X2' // TODO：相手のidを入れる
 
         const chatroomRef = collection(db, 'users', uid, 'chatroom', anotherId, 'chats');
-        const q = query(chatroomRef, orderBy('createTime', 'desc'), limit(10))
+        const q = query(chatroomRef, orderBy('createTime'), limit(50))
         const unsub = onSnapshot(q , (querySnapshot) => {
             setChats(
                 querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
             );
         });
-        console.log(chats)
     }, []);
-
 
     return (
         <div className='chatroom'>
@@ -40,7 +38,13 @@ const ChatRoom = () => {
                 <div className='header'>
                     <h2>ChatRoom</h2>
                 </div>
-                <div className='chat-screen'>
+                <div className='chat-screen'
+                    style={{
+                        width: '1000px',
+                        height: '600px',
+                        overflow: 'scroll',
+                    }}
+                >
                     <div className='message'>
                         {chats.map((chat: chatProps) => (
                             // eslint-disable-next-line react/jsx-key
