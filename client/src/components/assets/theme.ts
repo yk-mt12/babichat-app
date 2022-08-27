4ebe726f7a8a7805941311e170ed534c5edf4165
yk-mt12 <agvksl543@gmail.com>
2022-08-27 20:34:51 +0900

    theme.tsを作成 ブレイクポイントを使用可能

---
+++ client/src/components/assets/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 768,
+      lg: 1200,
+      xl: 1536,
+    },
+  },
+})
