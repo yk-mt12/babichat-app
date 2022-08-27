9346f423ec8982bdb6d70e962d6a509a3758fcbd
yk-mt12 <agvksl543@gmail.com>
2022-08-28 02:05:09 +0900

    ダッシュボード画面を作成した

--- client/src/components/pages/dashboard/Dashboard.tsx
+++ client/src/components/pages/dashboard/Dashboard.tsx
@@ -1,3 +1,5 @@
+import { Grid } from '@mui/material'
+import GridItem from '../../ui/gridItem/GridItem'
 import Message from '../message/Message'
 import Ranking from '../ranking/ranking'
 import Setting from '../setting/Setting'
@@ -7,10 +9,18 @@ import './Dashboard.css'
 const Dashboard = () => {
   return (
     <div className='dashboard--body'>
-      <TimeLine />
+      {/* <TimeLine />
       <Message />
       <Ranking />
-      <Setting />
+      <Setting /> */}
+      <Grid container direction='row' justifyContent='center' alignItems='flex-start'>
+        <GridItem child={<Ranking />} colRatio={6} />
+        <GridItem child={<TimeLine />} colRatio={6} />
+      </Grid>
+      <Grid container direction='row' justifyContent='center' alignItems='flex-start'>
+        <GridItem child={<Message />} colRatio={4} />
+        <GridItem child={<Setting />} colRatio={8} />
+      </Grid>
     </div>
   )
 }
