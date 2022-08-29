66a52c1d78c9f542b43f0b032be89de69c619da4
maple-fm <fuma0205boo@gmail.com>
2022-08-29 23:07:20 +0900

    修正

--- client/src/components/pages/message/MessageBox.tsx
+++ client/src/components/pages/message/MessageBox.tsx
@@ -4,6 +4,7 @@ import{ useState } from 'react'
 import { db } from '../../../firebase';
 import { useAuth } from '../../../firebase/authFunction';
 import SendIcon from '@mui/icons-material/Send';
+import { useParams } from 'react-router-dom';
 
 
 type ChatLog = {
@@ -19,6 +20,7 @@ const MessageBox = () => {
   const displayName = signInUser.displayName
   const uid = signInUser.uid
   const [msg, setMsg] = useState('');
+  const { anotherId } = useParams();
 
   const sendMsg = (e:any) => {
     e.preventDefault()
@@ -30,11 +32,11 @@ const MessageBox = () => {
       createTime: serverTimestamp()
     }
 
-    const anotherId = 'O1ujIkBZmJWXwdZi3htg5yai14X2' // TODO：相手のidを入れる
-    const chatroomRef = doc(db, 'users', uid, 'chatroom', anotherId);
+    // const anotherId = 'O1ujIkBZmJWXwdZi3htg5yai14X2' // TODO：相手のidを入れる
+    const chatroomRef = doc(db, 'users', uid, 'chatroom', anotherId||'');
     const chatsRef = collection(chatroomRef, 'chats')
 
-    const receiveChatroomRef = doc(db, 'users', anotherId, 'chatroom', uid);
+    const receiveChatroomRef = doc(db, 'users', anotherId||'', 'chatroom', uid);
     const receiveChatsRef = collection(receiveChatroomRef, 'chats')
 
     // console.log(chatroomRef)
