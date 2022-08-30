e44ddc9cdc94a970c9cec2d47650481d69943002
yk-mt12 <agvksl543@gmail.com>
2022-08-30 14:27:48 +0900

    ログアウトボタンを表示

--- client/src/components/ui/header/Header.tsx
+++ client/src/components/ui/header/Header.tsx
@@ -1,4 +1,5 @@
 import { useLocation } from 'react-router-dom'
+import SignOut from '../../model/user/SignOut'
 import './Header.css'
 
 type Props = {
@@ -12,6 +13,8 @@ const Header = (props: Props) => {
   return (
     <div>
       <h2 className={`title ${location.pathname === '/home' && 'when-home'}`}>{title}</h2>
+      {((location.pathname === '/home' && title === 'チャバットボ') ||
+        location.pathname !== '/home') && <SignOut />}
     </div>
   )
 }
