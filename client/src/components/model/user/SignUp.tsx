bdfedef57b46992fab258bae18f0f0fee1657192
kouki0215 <g2054128@cc.kyoto-su.ac.jp>
2022-09-03 02:39:58 +0900

    コメントアウト削除

--- client/src/components/model/user/SignUp.tsx
+++ client/src/components/model/user/SignUp.tsx
@@ -43,7 +43,6 @@ const SignUp = () => {
                   />
                 </div>
                 <div>
-                  {/* <label>パスワード</label> */}
                   <input
                     name='password'
                     type='password'
