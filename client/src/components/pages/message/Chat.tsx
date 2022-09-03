f5f978751705b5629748f02f289d1766cb3afa5c
maple-fm <fuma0205boo@gmail.com>
2022-09-03 15:54:29 +0900

    Chatに翻訳ボタンの追加

--- client/src/components/pages/message/Chat.tsx
+++ client/src/components/pages/message/Chat.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState }from 'react'
 import { useAuth } from '../../../firebase/authFunction'
 import { changeBabi } from '../../../logic/babigo'
 import './Chat.css'
@@ -16,13 +16,19 @@ const Chat = (props: chatProps) => {
   const signInUser = useAuth()
   const uid = signInUser.uid
   const babi = changeBabi(msg)
+  const [isClicked, setIsClicked] = useState(false)
+  const style = { margin: 0 }
 
   return (
     <div className={uid == sendid ? 'me' : 'you'}>
       {uid != sendid ? <p> { name }</p> : <p></p>}
-      <p className='says'>{babi}</p>
+      <p className='says'>{isClicked ? <p {...{ style }}>{msg} </p> : <p {...{ style }}>{babi}</p>}</p>
+      <p className='change' onClick={() => setIsClicked(!isClicked)}>
+        {isClicked ? 'バビ語' : '翻訳'}
+      </p>
     </div>
   )
 }
 
 export default Chat
+
