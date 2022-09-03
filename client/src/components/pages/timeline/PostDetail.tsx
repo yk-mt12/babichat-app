a89a0cd57bf7fdd83d4ed62aa03af2d30e62ac18
yk-mt12 <agvksl543@gmail.com>
2022-09-03 21:20:40 +0900

    レイアウトを作成した。

--- client/src/components/pages/timeline/PostDetail.tsx
+++ client/src/components/pages/timeline/PostDetail.tsx
@@ -1,5 +1,16 @@
+import { useParams } from 'react-router-dom'
+import GridItem from '../../ui/gridItem/GridItem'
+import Header from '../../ui/header/Header'
+
 const PostDetails = () => {
-  return <div>post detail</div>
+  const { postId } = useParams()
+
+  return (
+    <>
+      <Header title={`${postId}`} />
+      <GridItem colRatio={12} width='100vh' height='80vh' />
+    </>
+  )
 }
 
 export default PostDetails
