2bc0385ab30781aad4e667ba648d8e71a58139d7
yk-mt12 <agvksl543@gmail.com>
2022-09-03 18:14:15 +0900

    Propsのタイプ属性にコメントを追記

--- client/src/components/ui/gridItem/GridItem.tsx
+++ client/src/components/ui/gridItem/GridItem.tsx
@@ -3,15 +3,16 @@ import { Link } from 'react-router-dom'
 import './GridItem.css'
 
 type Props = {
-  child?: JSX.Element
-  colRatio: any
-  navigate?: string
-  width?: string | Number // gridカードの横幅
-  height?: string | Number // gridカードの縦
-  label?: string
-  cName?: string
-  isScroll?: boolean
+  child?:    JSX.Element        // コンポーネント
+  colRatio:  any                // Gridの比率
+  navigate?: string             // <Link/>の遷移先
+  width?:    string | Number    // gridカードの横幅
+  height?:   string | Number    // gridカードの縦
+  label?:    string             // グリッド内に表示するテキスト
+  cName?:    string             // className
+  isScroll?: boolean            // スクロール可能かどうか
 }
+
 const GridItem = (props: Props) => {
   const { child, colRatio, navigate, width, height, label, cName, isScroll } = props
   const navLink = navigate ? '/' + navigate : ''
