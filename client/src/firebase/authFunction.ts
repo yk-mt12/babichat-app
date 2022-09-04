fa84da9ef947d6d9935225eb36c7888647730fab
yk-mt12 <agvksl543@gmail.com>
2022-09-04 23:55:06 +0900

    usersテーブルにintroを追加

--- client/src/firebase/authFunction.ts
+++ client/src/firebase/authFunction.ts
@@ -60,6 +60,7 @@ export const useAuth = () => {
           photoURL: authUser.photoURL,
           createTime: serverTimestamp(),
           updateTime: serverTimestamp(),
+          intro: authUser.intro,
         })
       } else {
         resetStatus()
@@ -101,6 +102,7 @@ export const createUsersDB = async () => {
           likePostCount: 0,
           photoURL: authUser.photoURL ? authUser.photoURL : '',
           updateTime: serverTimestamp(),
+          intro: '',
         })
       }
     }
