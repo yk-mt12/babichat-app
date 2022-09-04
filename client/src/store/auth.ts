c5753842c7340ee3e761c5a3714002fb08389c05
yk-mt12 <agvksl543@gmail.com>
2022-09-04 23:55:06 +0900

    usersテーブルにintroを追加

--- client/src/store/auth.ts
+++ client/src/store/auth.ts
@@ -3,10 +3,11 @@ import { atom } from 'recoil'
 // TODO: displayName, photoURLの型をstringにしたいが、authFucntionでは、格納する際、型がstrirng | nullであるため、エラーを吐く
 type userState = {
   uid: string
-  displayName: any
-  photoURL: any
+  displayName: string | null
+  photoURL: string | null
   createTime: any
   updateTime: any
+  intro: string | null
 }
 
 export const signInUserState = atom<userState>({
@@ -17,5 +18,6 @@ export const signInUserState = atom<userState>({
     photoURL: '',
     createTime: '',
     updateTime: '',
+    intro: '',
   },
 })
