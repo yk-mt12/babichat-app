461a0f3562feeb6580f3ece55debb8c2f9a07529
maple-fm <fuma0205boo@gmail.com>
2022-09-05 11:13:20 +0900

    テストコメントを修正

--- client/src/_test_/Babigo.Test.ts
+++ client/src/_test_/Babigo.Test.ts
@@ -1,21 +1,24 @@
+// import { render, screen } from '@testing-library/react';
 import { changeBabi } from '../logic/babigo'
 
-test('babigo', () => {
-  const babigo = changeBabi('ありがとう')
-  expect(babigo).toBe('あばりびがばとぼうぶ')
-})
+describe('Test Change To BABIGO', () => {
+  test('babigo-normal', () => {
+    const babigo = changeBabi('ありがとう')
+    expect(babigo).toBe('あばりびがばとぼうぶ')
+  })
 
-test('babigo', () => {
-  const babigo = changeBabi('いぇーい')
-  expect(babigo).toBe('いびぇーいび')
-})
+  test('babigo-small+bar', () => {
+    const babigo = changeBabi('いぇーい')
+    expect(babigo).toBe('いびぇーいび')
+  })
 
-test('babigo', () => {
-  const babigo = changeBabi('ちゃっと')
-  expect(babigo).toBe('ちゃばっとぼ')
-})
+  test('babigo-small+small', () => {
+    const babigo = changeBabi('ちゃっと')
+    expect(babigo).toBe('ちゃばっとぼ')
+  })
 
-test('babigo', () => {
-  const babigo = changeBabi('今は、ひまです。')
-  expect(babigo).toBe('今はば、ひびまばでべすぶ。')
+  test('babigo include kannji', () => {
+    const babigo = changeBabi('今は、ひまです。')
+    expect(babigo).toBe('今はば、ひびまばでべすぶ。')
+  })
 })
