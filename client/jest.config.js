3d44cf94c67b03695b66a63a02c34dd7e0947471
yk-mt12 <agvksl543@gmail.com>
2022-09-05 12:16:34 +0900

    testの設定を変更した

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,15 +1,20 @@
 module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
   },
   globals: {
     'ts-jest': {
-      tsconfig: 'tsconfig.json',
+      tsconfig: 'tsconfig.test.json',
     },
   },
   moduleNameMapper: {
     '^~/(.+)': '<rootDir>/src/$1',
+    transform: {
+      '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
+    },
   },
   testMatch: ['<rootDir>/src/**/*.Test.ts', '<rootDir>/src/**/*.Test.tsx'],
 }
