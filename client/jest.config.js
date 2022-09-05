bc6c112be95e325ef93450546401e8bd3bab2a5e
yk-mt12 <agvksl543@gmail.com>
2022-09-05 13:36:34 +0900

    jest-transform-stubの設置を変更

--- client/jest.config.js
+++ client/jest.config.js
@@ -4,17 +4,11 @@ module.exports = {
   testEnvironment: 'node',
   moduleFileExtensions: ['js', 'ts', 'tsx'],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
   },
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json',
-    },
-  },
-  moduleNameMapper: {
-    '^~/(.+)': '<rootDir>/src/$1',
-    transform: {
-      '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     },
   },
   testMatch: ['<rootDir>/src/**/*.Test.ts', '<rootDir>/src/**/*.Test.tsx'],
