9457a618bd61165a3ef6840c10b8c5c025cb2abe
maple-fm <fuma0205boo@gmail.com>
2022-09-07 21:04:10 +0900

    流体シェイプの数を修正

--- client/src/components/ui/background/Fluid.tsx
+++ client/src/components/ui/background/Fluid.tsx
@@ -5,10 +5,12 @@ import './Fluid.css'
 function Fluid() {
   return (
     <div className='fluid'>
-      <BackgroundFluid top={2} rigth={2} deg={10} backgroundColor={'#fff100'} />
-      <BackgroundFluid top={40} rigth={60} deg={30} backgroundColor={'#fbad03'} />
-      <BackgroundFluid top={5} rigth={100} deg={90} backgroundColor={'#a3e417'} />
-      <BackgroundFluid top={60} rigth={120} deg={45} backgroundColor={'#ee6eee'} />
+      <BackgroundFluid top={2} rigth={2} deg={10} backgroundColor={'#fff199'} />
+      <BackgroundFluid top={40} rigth={60} deg={30} backgroundColor={'#fbad99'} />
+      <BackgroundFluid top={5} rigth={100} deg={90} backgroundColor={'#a3e499'} />
+      <BackgroundFluid top={60} rigth={120} deg={45} backgroundColor={'#dda0dd'} />
+      <BackgroundFluid top={70} rigth={3} deg={25} backgroundColor={'#b0c4de'} />
+      <BackgroundFluid top={80} rigth={85} deg={5} backgroundColor={'#fa89aa'} />
     </div>
   )
 }
